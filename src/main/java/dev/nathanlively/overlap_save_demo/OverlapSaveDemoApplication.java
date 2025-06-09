99161488d5bb6bf8ad372fd890494c131a47024c
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2025-06-09 09:24:25 -0500

    first commit

---
+++ src/main/java/dev/nathanlively/overlap_save_demo/OverlapSaveDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.nathanlively.overlap_save_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverlapSaveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverlapSaveDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nathanlively/overlap_save_demo/OverlapSaveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nathanlively.overlap_save_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverlapSaveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
